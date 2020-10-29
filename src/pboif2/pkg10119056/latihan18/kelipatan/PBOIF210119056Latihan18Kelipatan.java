5e1a3c23597ede443cd233e84737dc32bee75f73
ACER <ACER@Irfan>
2020-10-29 21:25:25 +0700

    upload project

---
+++ src/pboif2/pkg10119056/latihan18/kelipatan/PBOIF210119056Latihan18Kelipatan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119056.latihan18.kelipatan;
+
+/**
+ * @author ACER
+ * NAMA     : Irfan Ginanjar
+ * KELAS    : PBOIF2
+ * NIM      : 10119056
+ * Deskripsi Program: Program ini berisi program untuk menampilkan Kelipatan
+ */
+public class PBOIF210119056Latihan18Kelipatan {
+    public static void main(String[] args){
+        
+        for (double i=3.5;i<=35.5;i+=3.5){
+            System.out.println(i);
+        }
+    }
+}
